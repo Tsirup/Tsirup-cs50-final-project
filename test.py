22aa1a8b50f59fa6552eeb2a61c82f18b98049eb
Evan <frazierevanj@gmail.com>
2023-04-04 17:01:21 -0400

    packaged game

--- test.py
+++
@@ -1,3 +0,0 @@
-a = "test"
-b = "adasdjkaiatesadioahduwaduio"
-print(a in b)
--- units/pipeseam.lua
+++
@@ -1,9 +0,0 @@
-PipeSeam = Object:extend()
-
-function PipeSeam:new(y,x)
-    self.x = x
-    self.y = y
-    self.health = 99
-    self.order = 26
-    table.insert(UnitList, self)
-end
